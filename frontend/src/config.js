6071bdf85ce771266dc6bffbfe86c7b25bfbf7d5
DN-Apps <123365767+DN-Apps@users.noreply.github.com>
2025-10-29 13:07:08 +0100

    Feature/runtime config (#22)
    * Update Dockerfile
    
    Dockerfile for continous delivery
    
    * Create nginx-entrypoint.sh
    
    * Create config.template.js
    
    * Create config.ts
    
    * Update index.html
    
    * Update qa.yml
    
    * Update qa.yml
    
    409 error fix: separated artefact names
    
    * Update Login.js
    
    405 error fixing
    
    * Update Login.js
    
    path adjustment for config
    
    * Update Login.js
    
    crazy path definition, need to address this later to be build proof
    
    * Rename config.ts to config.js
    
    some hardening
    
    * Update Login.js
    
    antoher change in the path needed
    
    * Update config.js
    
    eslint error - tring to fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
-const runtimeCfg = (window as any).__APP_CONFIG__ || {};
+const runtimeCfg = (typeof window !== "undefined" && window.__APP_CONFIG__) ? window.__APP_CONFIG__ : {};
+
 export const API_BASE_URL =
   runtimeCfg.API_BASE_URL ||
   process.env.REACT_APP_API_BASE_URL ||
   process.env.REACT_APP_API_URL ||
-  "/api";
+  "/api"; 
